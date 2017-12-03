e062532775504ee4fb85c7d80853ac291005e7bd
jasmine <jasliu2012@gmail.com>
2017-12-02 18:11:35 -0600

    Downloaded vue for rails instead of requiring through src

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -1,31 +1,33 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    notes: [
-      {id: 1, body: "This is a first test", timestamp: Date.now()},
-      {id: 2, body: "This is a second test", timestamp: Date.now()},
-      {id: 3, body: "This is a third test", timestamp: Date.now()}
-    ]
-  },
-  methods: {
-    transformNotes: function(notes) {
-      return notes.slice().sort(function(a, b) {
-        return b.timestamp - a.timestamp
-      });
+/* global Vue */
+document.addEventListener("DOMContentLoaded", function(event) {
+  var app = new Vue({
+    el: '#app',
+    data: {
+      notes: [
+        {id: 1, body: "This is a first test", timestamp: Date.now()},
+        {id: 2, body: "This is a second test", timestamp: Date.now()},
+        {id: 3, body: "This is a third test", timestamp: Date.now()}
+      ]
     },
-    formatTitle: function(body) {
-      var maxLength = 20;
-      if (body.length > maxLength) {
-        return body.substring(0, maxLength - 3) + '...';
-      } else if (body.length === 0) {
-        return "New note";
-      } else {
-        return body;
+    methods: {
+      transformNotes: function(notes) {
+        return notes.slice().sort(function(a, b) {
+          return b.timestamp - a.timestamp
+        });
+      },
+      formatTitle: function(body) {
+        var maxLength = 20;
+        if (body.length > maxLength) {
+          return body.substring(0, maxLength - 3) + '...';
+        } else if (body.length === 0) {
+          return "New note";
+        } else {
+          return body;
+        }
+      },
+      formatTimestamp: function(timestamp) {
+        return new Date(timestamp).toUTCString();
       }
-    },
-    formatTimestamp: function(timestamp) {
-      return new Date(timestamp).toUTCString();
     }
-  }
+  });
 });
-
